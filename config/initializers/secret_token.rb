647611c69c659e070edf0d1b50d6e0865a1a47be
KryptykFysh <kryptykfysh@kryptykfysh.co.uk>
2014-01-13 16:22:03 +1100

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dresden::Application.config.secret_key_base = '990cfc5328fee352e843f3d49bca30b180ca3a1bbf729b43986e34a18f2f255693313217d3a7b0ed3076b276764b02a40a1d29bd8d71998ae8430811e1307a6b'
