402b8176634e99e325e6545e4c7778368df1807e
KryptykFysh <kryptykfysh@kryptykfysh.co.uk>
2014-02-05 21:24:04 +1100

    Character#show view improved. Create new character button now points to correct action.

--- spec/views/characters/show.html.haml_spec.rb
+++ spec/views/characters/show.html.haml_spec.rb
@@ -8,4 +8,11 @@ describe "characters/show.html.haml" do
 
   it { should have_title full_title(character.name) }
   it { should have_selector 'h5', character.name }
+  it { should have_link character.name, href: character_path(character) }
+  it { should have_selector 'dd', character.description }
+  it { should have_link character.campaign.name,
+    href: campaign_path(character.campaign) }
+  it { should have_selector 'dd', character.campaign.power_level.name }
+  it { should have_selector 'dd', character.high_concept.name }
+  it { should have_selector 'dd', character.trouble.name }
 end
